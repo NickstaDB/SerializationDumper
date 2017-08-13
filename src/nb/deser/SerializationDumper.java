110812aa54918746747582df8150af03fed73e2c
Nicky Bloor <insaneboarder234@gmail.com>
2017-08-13 01:10:43 +0100

    Initial commit.

---
+++ src/nb/deser/SerializationDumper.java
@@ -0,0 +1,15 @@
+package nb.deser;
+
+/***********************************************************
+ * 
+ * 
+ * Written by Nicky Bloor (@NickstaDB).
+ **********************************************************/
+public class SerializationDumper {
+	/*******************
+	 * 
+	 * @param args the command line arguments
+	 ******************/
+	public static void main(String[] args) {
+	}
+}
